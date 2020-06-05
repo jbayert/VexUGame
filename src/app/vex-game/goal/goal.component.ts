d410f4a18902562154f537be41a158ea5874a4c4
jbayert <35811092+jbayert@users.noreply.github.com>
2020-06-04 21:36:08 -0500

    goal css added
    Can change the colors of the goals using css and a few button call backs

--- src/app/vex-game/goal/goal.component.ts
+++ src/app/vex-game/goal/goal.component.ts
@@ -6,9 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goal.component.scss'],
 })
 export class GoalComponent implements OnInit {
+  ownedBy:"red"|"blue"|null;
 
-  constructor() { }
+  constructor() { 
+    this.ownedBy = null;
+  }
 
   ngOnInit() {}
+
+  setRedOwned(){
+    console.log("Success Red");
+    this.ownedBy = "red";
+  }
+
+  setBlueOwned(){
+    console.log("Success Blue");
+    this.ownedBy = "blue";
+  }
+
+  setUnowned(){
+    console.log("Success Unowned");
+    this.ownedBy = null;
+  }
 
 }
